013b3083355813769d093b0ba464d3abe87f7f1c
Les Aker <me@lesaker.org>
2015-01-11 23:16:47 -0800

    use delegator for array

--- lib/linodians/group.rb
+++ lib/linodians/group.rb
@@ -1,10 +1,19 @@
+require 'delegate'
+
 module Linodians
   ##
   # Group of employees
-  class Group
+  class Group < Delegator
     attr_reader :members
+    alias_method :__getobj__, :members
+
     def initialize
       @members = Linodians.download_data
+      super(@members)
+    end
+
+    def __setobj__(_)
+      @members
     end
   end
 end
