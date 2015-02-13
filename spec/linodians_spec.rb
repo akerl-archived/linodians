39948acefbebb4b4a63dca9cdc6efe26475eb08b
Les Aker <me@lesaker.org>
2015-02-12 23:23:36 -0500

    first test woo!

--- spec/linodians_spec.rb
+++ spec/linodians_spec.rb
@@ -1,1 +1,9 @@
 require 'spec_helper'
+
+describe Linodians do
+  describe '#new' do
+    it 'creates Group objects' do
+      expect(Linodians.new).to be_an_instance_of Linodians::Group
+    end
+  end
+end
