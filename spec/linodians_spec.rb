b7517f1f448937c270d96ed458b02131681db808
Les Aker <me@lesaker.org>
2015-02-12 23:56:16 -0500

    add vcr tests

--- spec/linodians_spec.rb
+++ spec/linodians_spec.rb
@@ -1,9 +1,29 @@
 require 'spec_helper'
 
+require 'json'
+
 describe Linodians do
   describe '#new' do
     it 'creates Group objects' do
-      expect(Linodians.new).to be_an_instance_of Linodians::Group
+      VCR.use_cassette('new_data') do
+        expect(Linodians.new).to be_an_instance_of Linodians::Group
+      end
+    end
+  end
+
+  describe '#load_data' do
+    let(:saved_data) { File.open('spec/examples/data') { |fh| JSON.load fh } }
+    let(:new_data) { VCR.use_cassette('new_data') { Linodians.load_data } }
+
+    it 'downloads data from linode.com' do
+      expect(new_data).to be_an_instance_of Array
+      expect(new_data.first).to be_an_instance_of Linodians::Employee
+    end
+
+    it 'accepts data as input' do
+      data = Linodians.load_data(saved_data)
+      expect(data).to be_an_instance_of Array
+      expect(data.first).to be_an_instance_of Linodians::Employee
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,9 @@ end
 
 require 'rspec'
 require 'linodians'
+
+require 'vcr'
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/cassettes'
+  c.hook_into :webmock
+end
